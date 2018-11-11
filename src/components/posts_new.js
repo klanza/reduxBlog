872c2e123cbb09899536cb35f1c27fba8c442956
klanza <klanza91@gmail.com>
2018-11-10 20:14:40 -0700

    Added field information, testing

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+    return (
+      <div>
+        <input type="text" {...field.input} />
+      </div>
+    );
+  }
+
   render() {
-    return <div />;
+    return (
+      <form>
+        <Field
+          name="title"
+          // reference to function only, no parentheses. Field will call that function at some point
+          component={this.renderTitleField}
+        />
+      </form>
+    );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+// Sets up alias for reducer, redux-form piece is just called "reducuer", this prevents conflict
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
+
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
