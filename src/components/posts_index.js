452460ac01b8805d5f7d676695da307ebb100e89
klanza <klanza91@gmail.com>
2018-11-10 19:14:08 -0700

    Added new posts component, hit API route

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
@@ -7,13 +8,32 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
-  render() {
-    return <div>Posts Index</div>;
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    });
   }
+
+  render() {
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
 }
 
 // Shortcut instead of binding mapDispatchToProps
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostsIndex);
