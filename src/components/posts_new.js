568c380ff6d105e130492ab02c3b13811bf17b1f
klanza <klanza91@gmail.com>
2018-12-08 23:43:37 -0700

    Add error handling to form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -25,8 +25,8 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <Field
-          label="Tags"
-          name="tags"
+          label="Categories"
+          name="categories"
           component={this.renderField}
         />
 
@@ -40,6 +40,28 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  
+  const errors = {};
+
+  // Validate the inputs from 'values'
+
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories.";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content please."
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If errors has *any* propertires, redux-form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
