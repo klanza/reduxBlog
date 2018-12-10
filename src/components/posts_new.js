6cf6b0f4d551c16dae4b4e533e11f791f5770ecb
klanza <klanza91@gmail.com>
2018-12-09 23:43:50 -0700

    Add navigation buttons, styling, and post submission to API

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions'
+
 
 class PostsNew extends Component {
   renderField(field) {
+    // Destructuring
+    const { meta: {touched, error} } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
         className="form-control"
         type="text"
         {...field.input}
         />
-        {field.meta.error}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
 
+  // When hitting APIs think in action creators
   onSubmit(values) {
-    console.log(values);
+    // this === component
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -44,6 +57,9 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link className="btn btn-danger" to="/">
+          Cancel
+        </Link>
       </form>
     );
   }
@@ -73,4 +89,6 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost }) (PostsNew)
+);
